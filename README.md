7bc7a4d0a10f751b55f3a7e41cbc0f4399f657cf
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2022-05-06 19:00:15 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
 [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 [![Imports: isort](https://img.shields.io/badge/%20imports-isort-%231674b1?style=flat&labelColor=ef8336)](https://pycqa.github.io/isort/)
 
-# template-python
-
-Template python repository.
-
-The folder name `template` should be changed to the package name.
-Entries in `setup.py` should be adapted to the path(s)/URL(s)/name(s)/...
-involved in the package.
+# NeuroFeedback demo
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 # Version
 version = None
-with open(Path(__file__).parent/'template'/'_version.py', 'r') as file:
+with open(Path(__file__).parent/'demo_nfb'/'_version.py', 'r') as file:
     for line in file:
         line = line.strip()
         if line.startswith('__version__'):
@@ -15,7 +15,7 @@ if version is None:
     raise RuntimeError('Could not determine version.')
 
 # Descriptions
-short_description = """Short template package description"""
+short_description = """Demo for neurofeedback"""
 long_description_file = Path(__file__).parent / 'README.md'
 with open(long_description_file, 'r') as file:
     long_description = file.read()
@@ -32,12 +32,12 @@ DESCRIPTION = short_description
 LONG_DESCRIPTION = long_description
 LONG_DESCRIPTION_CONTENT_TYPE=long_description_content_type
 AUTHOR = 'Mathieu Scheltienne'
-AUTHOR_EMAIL = 'mathieu.scheltienne@gmail.com'
+AUTHOR_EMAIL = 'mathieu.scheltienne@fcbg.ch'
 MAINTAINER = 'Mathieu Scheltienne'
-MAINTAINER_EMAIL = 'mathieu.scheltienne@gmail.com'
-URL = 'https://github.com/mscheltienne/template-python'
+MAINTAINER_EMAIL = 'mathieu.scheltienne@fcbg.ch'
+URL = 'https://github.com/mscheltienne/demo-nfb'
 LICENSE = 'MIT License'
-DOWNLOAD_URL = 'https://github.com/mscheltienne/template-python'
+DOWNLOAD_URL = 'https://github.com/mscheltienne/demo-nfb'
 VERSION = version
 
 
@@ -83,9 +83,9 @@ setup(
         ],
     keywords='neuroscience neuroimaging',
     project_urls={
-        'Documentation': 'https://github.com/mscheltienne/template-python',
-        'Source': 'https://github.com/mscheltienne/template-python',
-        'Tracker': 'https://github.com/mscheltienne/template-python/issues'
+        'Documentation': 'https://github.com/mscheltienne/demo-nfb',
+        'Source': 'https://github.com/mscheltienne/demo-fb',
+        'Tracker': 'https://github.com/mscheltienne/demo-nfb/issues'
         },
     platforms='any',
     python_requires='>=3.7',
