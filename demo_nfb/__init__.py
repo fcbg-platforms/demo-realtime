c089c70b6173b8512433911c40ed41c75f0775ea
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-09-22 15:23:42 +0200

    add missing import

--- demo_nfb/__init__.py
+++ demo_nfb/__init__.py
@@ -1,6 +1,7 @@
 from ._version import __version__  # noqa: F401
 from .fft import fft  # noqa: F401
 from .nfb import basic  # noqa: F401
+from .utils._config import sys_info  # noqa: F401
 from .utils._logs import (  # noqa: F401
     add_file_handler,
     add_stream_handler,
