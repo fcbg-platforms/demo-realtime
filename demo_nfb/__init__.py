ad73ee1820066167f8865245d7c6e89100955f50
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-09-22 14:48:37 +0200

    update init

--- demo_nfb/__init__.py
+++ demo_nfb/__init__.py
@@ -1,4 +1,10 @@
 from ._version import __version__  # noqa: F401
 from .fft import fft  # noqa: F401
 from .nfb import basic  # noqa: F401
-from .utils._logs import logger, set_log_level  # noqa: F401
+from .utils._logs import (  # noqa: F401
+    add_file_handler,
+    add_stream_handler,
+    logger,
+    set_handler_log_level,
+    set_log_level,
+)
