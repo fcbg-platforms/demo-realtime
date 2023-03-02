7ba66a26643b35998792e48239a8f18008f97f3c
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-03-02 23:35:24 +0100

    Fix CIs (#3)
    * try to increase delays
    
    * try step by step
    
    * the hell
    
    * goooo alllllll the way back
    
    This reverts commit b9ec4344f96e8a776186ab1bdac7774e0a92684a.
    
    Revert "the hell"
    
    This reverts commit f5dfdc09bd804d263708d3828f1208803b0b1bcf.
    
    Revert "try step by step"
    
    This reverts commit 4f9778f47991e7c54f9d007cbfa9bb7beec8c037.
    
    Revert "try to increase delays"
    
    This reverts commit da7ee897d7ffd6fbeb7f4c95905927c4a47a43a8.
    
    Revert "deduplicate code"
    
    This reverts commit 87049b9688e116969440d7e0bd8cad6a8d30c323.
    
    Revert "enable restart of the car game"
    
    This reverts commit 4d770cf5b4eeb0bb1f34a53d83af2823cd11a1ad.
    
    Revert "fix raycast direction"
    
    This reverts commit 3c28dec5fef00a91bc978b525fafc26e1fed4ec6.
    
    Revert "fix tests"
    
    This reverts commit 396bca75c96bf8933a9744fda6905d5221847e47.
    
    Revert "add sleep between stat and first command"
    
    This reverts commit b7d9da2b11dcbe33c236734d6ba954bbc411eb13.
    
    Revert "don't run go_left and right if process is dead"
    
    This reverts commit f8bb859a7f3a9c93e145129747ef60560bc6637b.
    
    * try this
    
    * try this
    
    * and this
    
    * add this
    
    * add sleep
    
    * block until process is launched
    
    * block also for double spinning wheel feedback
    
    * add comment
    
    * the hell..
    
    * what about this
    
    * remove bad code
    
    * enable test back
    
    * give time to spawn process

--- demo_realtime/feedbacks/tests/test_car_game.py
+++ demo_realtime/feedbacks/tests/test_car_game.py
@@ -11,7 +11,7 @@ logger.propagate = True
 
 
 @requires_missing_ursina
-def test_missing_psychopy():
+def test_missing_ursina():
     """Test error if Ursina is missing."""
     with pytest.raises(ImportError, match="optional dependency 'ursina'"):
         CarGame()
@@ -22,42 +22,35 @@ def test_car_game(caplog):
     """Test the car game feedback."""
     game = CarGame()
     game.start()
-
-    time.sleep(1)
+    time.sleep(4)
+    assert game._process.is_alive()
     game.go_right()
     assert game.direction == "right"
     caplog.clear()
-
     game.go_right()
     assert game.direction == "right"
     assert "Already going right." in caplog.text
     caplog.clear()
-
     time.sleep(1)
     game.go_right()
     assert game.direction == "right"
-    assert "Already going right." not in caplog.text
-    caplog.clear()
-
     game.go_left()
     assert "Already going right." in caplog.text
-    caplog.clear()
-
+    time.sleep(1)
     game.stop()
-
     time.sleep(1)
+    caplog.clear()
+    game.go_right()
+    assert "is not running" in caplog.text
+    caplog.clear()
     game.go_left()
-    assert "The game is not running." in caplog.text
+    assert "is not running" in caplog.text
     caplog.clear()
-
-    game.go_right()
-    assert "The game is not running." in caplog.text
-    caplog.clear()
-
     game.start()
-    time.sleep(1)
-    assert game.direction == "straight"
-    game.go_right()
-    assert game.direction == "right"
-    assert "Already going right." not in caplog.text
+    time.sleep(4)
+    assert game._process.is_alive()
+    game.go_left()
+    assert "Already going left." not in caplog.text
+    game.go_left()
+    assert "Already going left." in caplog.text
     game.stop()
