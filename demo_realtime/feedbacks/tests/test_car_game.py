990c26a812713f1d7c8d4896cac10b5dfebed5d9
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2023-03-02 23:14:29 +0100

    enable test back

--- demo_realtime/feedbacks/tests/test_car_game.py
+++ demo_realtime/feedbacks/tests/test_car_game.py
@@ -22,16 +22,17 @@ def test_car_game(caplog):
     """Test the car game feedback."""
     game = CarGame()
     game.start()
+    time.sleep(1)
     assert game._process.is_alive()
-    # game.go_right()
-    # assert game.direction == "right"
-    # caplog.clear()
-    # game.go_right()
-    # assert game.direction == "right"
-    # assert "Already going right." in caplog.text
-    # caplog.clear()
-    # time.sleep(1)
-    # game.go_right()
-    # assert game.direction == "right"
-    # game.go_left()
-    # assert "Already going right." in caplog.text
+    game.go_right()
+    assert game.direction == "right"
+    caplog.clear()
+    game.go_right()
+    assert game.direction == "right"
+    assert "Already going right." in caplog.text
+    caplog.clear()
+    time.sleep(1)
+    game.go_right()
+    assert game.direction == "right"
+    game.go_left()
+    assert "Already going right." in caplog.text
