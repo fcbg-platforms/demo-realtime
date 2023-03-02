b7d9da2b11dcbe33c236734d6ba954bbc411eb13
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-03-02 17:43:37 +0100

    add sleep between stat and first command

--- demo_realtime/feedbacks/tests/test_car_game.py
+++ demo_realtime/feedbacks/tests/test_car_game.py
@@ -22,7 +22,7 @@ def test_car_game(caplog):
     """Test the car game feedback."""
     game = CarGame()
     game.start()
-
+    time.sleep(1)
     game.go_right()
     assert game.direction == "right"
     caplog.clear()
